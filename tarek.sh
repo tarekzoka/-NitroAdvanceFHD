0b2c45e846d1da4043b4a198ba19c83f736ee9c8
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2024-03-06 23:57:09 +0200

    Add files via upload

--- tarek.sh
+++ tarek.sh
@@ -1,55 +1,12 @@
-#!/bin/bash
-######################################################################################
-## Command=wget https://raw.githubusercontent.com/tarekzoka/NitroAdvanceFHD/main/tarek.sh -O - | /bin/sh
-###########################################
-###########################################
 #!/bin/sh
-echo
-opkg install --force-overwrite  https://drive.google.com/uc?id=1N1A3p80kT7Xd7m3sSIdZniL02Ggh68xE&export=download
-wait
+#
+
+wget -O /tmp/NitroadvanceFHD_v5.2.tar.gz "https://raw.githubusercontent.com/tarekzoka/NitroAdvanceFHD/main/NitroadvanceFHD_v5.2.tar.gz"
+
+tar -xzf /tmp/*.tar.gz -C /
+
+rm -r /tmp/NitroadvanceFHD_v5.2.tar.gz 
+
+killall -9 enigma2
+
 sleep 2;
-exit 0
-
-#########################################################
-MY_MAIN_URL="https://raw.githubusercontent.com/tarekzoka/"
-MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_FILE
-MY_TMP_FILE="/tmp/"$MY_FILE
-
-rm -f $MY_TMP_FILE > /dev/null 2>&1
-
-MY_SEP='============================================================='
-echo $MY_SEP
-echo 'Downloading '$MY_FILE' ...'
-echo $MY_SEP
-echo ''
-wget -T 2 $MY_URL -P "/tmp/"
-
-if [ -f $MY_TMP_FILE ]; then
-
-	echo ''
-	echo $MY_SEP
-	echo 'Extracting ...'
-	echo $MY_SEP
-	echo ''
-	tar -xf $MY_TMP_FILE -C /
-	MY_RESULT=$?
-
-	rm -f $MY_TMP_FILE > /dev/null 2>&1
-
-	echo ''
-	echo ''
-	if [ $MY_RESULT -eq 0 ]; then
-        echo "#########################################################"
-        echo "#  ###################### Skin $version INSTALLED SUCCESSFULLY      #"
-        echo "#                BY TAREK_TT - support on                   #"
-        echo "#   .................................................      #"
-        echo "#########################################################"
-        echo "#           your Device will RESTART Now                #"
-        echo "#########################################################"
-wait
-sleep 2;
-exit 0
-
-
-
-
